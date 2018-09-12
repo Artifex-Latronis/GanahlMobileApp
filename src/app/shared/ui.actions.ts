36e75ea2a36ffd94ee59fdb3b2821916dfb3044f
Artifex-Latronis <Artifex-Latronis@users.noreply.github.com>
2018-09-12 11:01:24 -0700

    laying foundation to the NgRx store

--- src/app/shared/ui.actions.ts
+++ src/app/shared/ui.actions.ts
@@ -4,6 +4,10 @@ export const START_LOADING = '[UI] Start Loading';
 export const STOP_LOADING = '[UI] Stop Loading';
 export const START_DISPLAYING_UNIT = '[UI] Start Displaying Unit';
 export const STOP_DISPLAYING_UNIT = '[UI] Stop Displaying Unit';
+export const START_PULLING_UNIT = '[UI] Start Pulling Unit';
+export const STOP_PULLING_UNIT = '[UI] Stop Pulling Unit';
+export const START_MOVING_UNIT = '[UI] Start moving Unit';
+export const STOP_MOVING_UNIT = '[UI] Stop moving Unit';
 
 export class StartLoading implements Action {
   readonly type = START_LOADING;
@@ -21,4 +25,30 @@ export class StopDisplayingUnit implements Action {
   readonly type = STOP_DISPLAYING_UNIT;
 }
 
-export type UIActions = StartLoading | StopLoading | StartDisplayingUnit | StopDisplayingUnit;
+export class StartPullingUnit implements Action {
+  readonly type = START_PULLING_UNIT;
+}
+
+export class StopPullingUnit implements Action {
+  readonly type = STOP_PULLING_UNIT;
+}
+
+export class StartMovingUnit implements Action {
+  readonly type = START_MOVING_UNIT;
+}
+
+export class StopMovingUnit implements Action {
+  readonly type = STOP_MOVING_UNIT;
+}
+
+export type UIActions =
+  StartLoading |
+  StopLoading |
+  StartDisplayingUnit |
+  StopDisplayingUnit |
+  StartPullingUnit |
+  StopPullingUnit |
+  StartMovingUnit |
+  StopMovingUnit;
+
+
--- src/app/shared/ui.reducers.ts
+++ src/app/shared/ui.reducers.ts
@@ -1,13 +1,27 @@
-import { UIActions, START_LOADING, STOP_LOADING, START_DISPLAYING_UNIT, STOP_DISPLAYING_UNIT } from './ui.actions';
+import {
+  UIActions,
+  START_LOADING,
+  STOP_LOADING,
+  START_DISPLAYING_UNIT,
+  STOP_DISPLAYING_UNIT,
+  START_PULLING_UNIT,
+  STOP_PULLING_UNIT,
+  START_MOVING_UNIT,
+  STOP_MOVING_UNIT
+} from './ui.actions';
 
 export interface State {
   isLoading: boolean;
   isDisplayingUnit: boolean;
+  isPullingUnit: boolean;
+  isMovingUnit: boolean;
 }
 
 const initialState: State = {
   isLoading: false,
-  isDisplayingUnit: false
+  isDisplayingUnit: false,
+  isPullingUnit: false,
+  isMovingUnit: false
 };
 
 export function uiReducer(state = initialState, action: UIActions) {
@@ -32,6 +46,26 @@ export function uiReducer(state = initialState, action: UIActions) {
         ...state,
         isDisplayingUnit: false
       };
+    case START_PULLING_UNIT:
+      return {
+        ...state,
+        isPullingUnit: true
+      };
+    case STOP_PULLING_UNIT:
+      return {
+        ...state,
+        isPullingUnit: false
+      };
+    case START_MOVING_UNIT:
+      return {
+        ...state,
+        isMovingUnit: true
+      };
+    case STOP_MOVING_UNIT:
+      return {
+        ...state,
+        isMovingUnit: false
+      };
     default: {
       return state;
     }
@@ -40,3 +74,5 @@ export function uiReducer(state = initialState, action: UIActions) {
 
 export const getIsLoading = (state: State) => state.isLoading;
 export const getIsDisplayingUnit = (state: State) => state.isDisplayingUnit;
+export const getIsPullingUnit = (state: State) => state.isPullingUnit;
+export const getIsMovingUnit = (state: State) => state.isMovingUnit;
